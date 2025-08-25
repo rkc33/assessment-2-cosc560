b5c98090f8088d59a441b8185afe3f225b3875f7
Rabi Kumar K C <rkc3@myune.edu.au>
2025-08-25 23:33:42 +1000

    creaetd models, migrations and relationships

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+     use HasFactory;
+
+    protected $fillable = [
+        'name', 'content'
+    ];
+
+    // Relationships
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
